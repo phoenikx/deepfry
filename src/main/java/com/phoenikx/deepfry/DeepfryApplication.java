b506cf1df8c399f5d4821bbbc1fe94f2485158da
phoenikx <nik211012@gmail.com>
2020-03-25 20:25:31 +0530

    Initial commit

---
+++ src/main/java/com/phoenikx/deepfry/DeepfryApplication.java
@@ -0,0 +1,13 @@
+package com.phoenikx.deepfry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepfryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepfryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenikx/deepfry/DeepfryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenikx.deepfry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepfryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
